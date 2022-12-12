de86e37ec13a27456de4fbe0c47a4fa91da12998
RAVAGER206 <114873479+RAVAGER206@users.noreply.github.com>
2022-12-12 13:41:53 +0300

    Add files via upload

---
+++ MIDLE.h
@@ -0,0 +1,36 @@
+#ifndef MIDDLE_H_INCLUDED
+#define MIDDLE_H_INCLUDED
+
+long long perevorot(long long i);
+
+int itc_min_num(long long a);
+
+int itc_rev_num(long long a);
+
+int itc_null_count(long long a);
+
+bool itc_mirror_num(long long a);
+
+int itc_mirror_count(long long a);
+
+void itc_num_print(long a);
+
+int itc_len_num(long long a);
+
+int itc_sum_num(long long a);
+
+long long itc_multi_num(long long a);
+
+int itc_max_num(long long a);
+
+int itc_second_max_num(long long a);
+
+int itc_second_simple_max_num(long long a);
+
+long long itc_bin_num(long long a);
+
+long long itc_oct_num(long long a);
+
+int itc_rev_bin_num(long long a);
+
+#endif
