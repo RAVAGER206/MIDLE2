5608e70c0daba535af75dcff3bb1c93e479b3657
RAVAGER206 <114873479+RAVAGER206@users.noreply.github.com>
2022-12-12 13:42:40 +0300

    Add files via upload

---
+++ MIDLE4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include"MIDLE.h"
+using namespace std;
+
+
+int itc_rev_bin_num(long long a){ // 16
+int d, i, nch;
+i = 1;
+nch = 0;
+while (a > 0){
+d = a % 10;
+nch = nch + d*i;
+i = i * 2;
+a = a / 10;
+}
+return nch;
+}
